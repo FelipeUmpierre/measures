7e9a7e13dd3946b6fcfcf2193c37aea9bcb305a6
Felipe Pieretti Umpierre <felipe.pieretti@solarisbank.de>
2018-03-27 14:01:26 +0200

    Update repository

--- pkg/repository/users.go
+++ pkg/repository/users.go
@@ -24,15 +24,14 @@ func NewUsersRepository(db *sqlx.DB) *UserRepo {
 }
 
 // Save saves the domain
-func (u *UserRepo) Save(user domain.User) (domain.User, error) {
-	builder := lk.Insert().Set(
+func (u *UserRepo) Save(user *domain.User) (*domain.User, error) {
+	query, args := lk.Insert(`users`).Set(
 		lk.Pair(`id`, user.ID),
 		lk.Pair(`name`, user.Name),
 	).OnConflict(`id`, lk.DoUpdate(
-		lk.Pair(`name`, user.Name)
-	)).Returning(`id`, `name`)
-
-	query, args := builder.Prepare()
+		lk.Pair(`name`, user.Name),
+	)).Returning(`id`, `name`).
+		Prepare()
 
 	stmt, err := u.db.PrepareNamed(query)
 	if err != nil {
@@ -40,12 +39,12 @@ func (u *UserRepo) Save(user domain.User) (domain.User, error) {
 	}
 	defer stmt.Close()
 
-	err = stmt.Get(&user, args...)
+	err = stmt.Get(user, args)
 	return user, err
 }
 
 // FindAll returns the result for all rows
-func (u *UserRepo) FindAll() ([]domain.User, error) {
+func (u *UserRepo) FindAll() (*[]domain.User, error) {
 	query, args := lk.Select(`id`, `name`).From(`users`).Prepare()
 
 	stmt, err := u.db.PrepareNamed(query)
@@ -55,14 +54,17 @@ func (u *UserRepo) FindAll() ([]domain.User, error) {
 	defer stmt.Close()
 
 	users := new([]domain.User)
-	err = stmt.Select(users, args...)
+	err = stmt.Select(users, args)
 
 	return users, err
 }
 
 // FindByID return the result for specific row
 func (u *UserRepo) FindByID(ID string) (*domain.User, error) {
-	query, args := lk.Select(`id`, `name`).From(`users`).Where(lk.Condition(`id`).Equal(ID)).Prepare()
+	query, args := lk.Select(`id`, `name`).
+		From(`users`).
+		Where(lk.Condition(`id`).Equal(ID)).
+		Prepare()
 
 	stmt, err := u.db.PrepareNamed(query)
 	if err != nil {
@@ -71,7 +73,7 @@ func (u *UserRepo) FindByID(ID string) (*domain.User, error) {
 	defer stmt.Close()
 
 	user := new(domain.User)
-	if err = u.db.Get(user, args...); err != nil {
+	if err = stmt.Get(user, args); err != nil {
 		return nil, err
 	}
 
