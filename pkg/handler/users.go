ad884c0c6f79965d9953f84b1e20a0f27ec500f8
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2018-03-29 00:15:46 +0200

    Introduce logic for handler to get and post

--- pkg/handler/users.go
+++ pkg/handler/users.go
@@ -3,18 +3,58 @@ package handler
 import (
 	"net/http"
 
+	"github.com/go-chi/chi"
+
 	"github.com/FelipeUmpierre/measures/pkg/domain"
+	"github.com/go-chi/render"
 )
 
 type (
+	findAllUserRepository interface {
+		FindAll() (*[]domain.User, error)
+	}
+
 	getUserRepository interface {
-		FindAll() ([]domain.User, error)
+		FindByID(ID string) (*domain.User, error)
 	}
 )
 
 // AllUsers return the users saved
-func AllUsers(repo getUserRepository) func(w http.ResponseWriter, r *http.Request) {
+func AllUsers(repo findAllUserRepository) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
+		users, err := repo.FindAll()
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			render.JSON(w, r, struct {
+				Error string `json:"error"`
+			}{
+				err.Error(),
+			})
 
+			return
+		}
+
+		render.JSON(w, r, users)
+	}
+}
+
+// GetUser return the users saved
+func GetUser(repo getUserRepository) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		id := chi.URLParam(r, `id`)
+
+		user, err := repo.FindByID(id)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			render.JSON(w, r, struct {
+				Error string `json:"error"`
+			}{
+				err.Error(),
+			})
+
+			return
+		}
+
+		render.JSON(w, r, user)
 	}
 }
