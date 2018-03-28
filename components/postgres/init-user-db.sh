5d15b0e63ca6d045887599bf5eb47849d7bcddfc
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2018-03-29 00:09:26 +0200

    Docker changes

--- components/postgres/init-user-db.sh
+++ components/postgres/init-user-db.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER felipe;
-    CREATE DATABASE measures;
-    GRANT ALL PRIVILEGES ON DATABASE measures TO felipe;
-EOSQL
+# createuser 'felipe'
+# createdb 'measures' -O 'felipe'
--- components/postgres/init.sql
+++ components/postgres/init.sql
@@ -1,13 +1,20 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
+DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE users (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
     name VARCHAR,
     UNIQUE(id)
 );
 
+DROP TABLE IF EXISTS measures CASCADE;
 CREATE TABLE measures (
-    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    measures JSON,
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v1(),
+    handle VARCHAR NOT NULL,
+    aggregate_id UUID NOT NULL,
+    payload JSON,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     UNIQUE(id)
 );
+
+INSERT INTO users (name) VALUES ('Felipe'), ('Vivian');
