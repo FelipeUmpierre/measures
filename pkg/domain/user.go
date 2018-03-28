28d95ab6cbd577f5c2c4b0e501b5a10eb9493ceb
Felipe Pieretti Umpierre <umpierre.felipe@gmail.com>
2018-03-29 00:15:57 +0200

    Update struct to have uuid

--- pkg/domain/user.go
+++ pkg/domain/user.go
@@ -1,9 +1,11 @@
 package domain
 
+import uuid "github.com/satori/go.uuid"
+
 type (
 	// User holds the domain data
 	User struct {
-		ID   int    `db:"id" json:"id"`
-		Name string `db:"name" json:"name"`
+		ID   uuid.UUID `db:"id" json:"id"`
+		Name string    `db:"name" json:"name"`
 	}
 )
